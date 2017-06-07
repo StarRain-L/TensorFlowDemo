15b9ea2dad3c44409164cc5a2ad4121cfca60fa0
liusifang <liusifang@jd.com>
2017-06-07 13:56:05 +0800

    初始化
    初始化

---
+++ TensorFlowDemo/TensorflowVC.h
@@ -0,0 +1,14 @@
+//
+//  TensorflowVC.h
+//  PictureOCR
+//
+//  Created by jr on 2017/6/2.
+//  Copyright © 2017年 jr. All rights reserved.
+//
+#import <AVFoundation/AVFoundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface TensorflowVC : UIViewController
+
+@end
---
+++ TensorFlowDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TensorFlowDemo
+//
+//  Created by jr on 2017/6/5.
+//  Copyright © 2017年 jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
